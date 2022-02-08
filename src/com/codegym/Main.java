5e09c45d3b17a0508291d468b4a5184d3890c4d8
DuongXuanBach <duongxuanbach3112@gmail.com>
2022-02-08 14:41:37 +0700

    add file

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -9,17 +9,17 @@ public class Main {
         int month = 1;
         double interestRate = 1.0;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter investment amount:");
+        System.out.println("Enter investment amount: ");
         money = input.nextDouble();
-        System.out.println("Enter annual interest rate in percentage:");
-        interestRate = input.nextDouble();
-        System.out.println("Enter number of months:");
+        System.out.println("Enter number of months: ");
         month = input.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interestRate = input.nextDouble();
         double totalInterest = 0;
-        for (int i = 0; i < month; i++) {
-            totalInterest += money * (interestRate/100)/12 * month;
+        for(int i = 0; i < month; i++){
+            totalInterest += money * (interestRate/100)/12 ;
         }
-        System.out.println("Total interestRate is :" + interestRate);
-	// write your code here
+        System.out.println("Total of interest: " + totalInterest);
+
     }
 }
